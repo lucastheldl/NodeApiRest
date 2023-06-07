29be537dba3f8838c9c52dfecfcee69a2c948922
lucastheldl <lucastheldl@gmail.com>
2023-06-07 08:10:46 -0300

    initial commit, knex config

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+import { knex } from "./database";
+
+const app = fastify();
+
+app.get("/", async () => {
+  const test = await knex("sqlite_schema").select("*");
+
+  return test;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("server running");
+  });
