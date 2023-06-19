e380d106074b69e3531bbbdb76ab4047eebfc03b
lucastheldl <lucastheldl@gmail.com>
2023-06-19 14:01:23 -0300

    feat: add knex data types

--- db/migrations/20230614185702_add-session-id-to-transactions.ts
+++ db/migrations/20230614185702_add-session-id-to-transactions.ts
@@ -2,6 +2,7 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable("transactions", (table) => {
+    //opcional por nao ter o .notNullable()
     table.uuid("session_id").after("id").index();
   });
 }
